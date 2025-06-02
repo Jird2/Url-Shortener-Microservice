13a46aa93a8f73a492528482b494c1d4dc8bb5a3
Jird2 <jpaperny123@gmail.com>
2025-06-02 11:33:46 -0400

    prevent duplicate short_url entries

--- index.js
+++ index.js
@@ -5,19 +5,17 @@ const app = express();
 const dns = require("dns").promises;
 const bodyparse = require("body-parser");
 const mongoose = require("mongoose");
-// Basic Configuration
 const port = process.env.PORT || 3000;
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 const schema = new mongoose.Schema({
-  ogurl: String,
-  shortened: Number
+  original_url: {type: String, unique: true},
+  short_url: Number
 });
 let Url = mongoose.model("Url", schema);
 app.use(cors());
-// Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
@@ -27,7 +25,10 @@ app.get('/', function(req, res) {
 });
 app.use(bodyparse.urlencoded({extended:false}));
 app.post('/api/shorturl', async function(req, res) {
-  const rawurl = req.body.url;
+  var rawurl = req.body.url.trim();
+  if (rawurl.endsWith("/")) {
+    rawurl = rawurl.slice(0,-1);
+  }
   let host;
   try {
     host = new URL(rawurl).hostname;
@@ -42,18 +43,18 @@ app.post('/api/shorturl', async function(req, res) {
     return res.json({error: 'Invalid Url' });
   }
   try {
-    let seen = await Url.findOne({ogurl: rawurl });
+    let seen = await Url.findOne({original_url: rawurl });
     if (seen) {
       return res.json({
-        ogurl: seen.ogurl, shortened: seen.shortened});
+        original_url: seen.original_url, short_url: seen.short_url});
     }  
     var count = await Url.countDocuments({});
     var plusone = new Url({
-      ogurl: rawurl, shortened: count+1
+      original_url: rawurl, short_url: count+1
     });
     const saved = await plusone.save();
     return res.json({
-      ogurl: saved.ogurl, shortened: saved.shortened
+      original_url: saved.original_url, short_url: saved.short_url
     });
   } 
   catch (err) {
@@ -64,11 +65,11 @@ app.post('/api/shorturl', async function(req, res) {
 app.get('/api/shorturl/:num', async function(req, res) {
   const val = parseInt(req.params.num);
   try {
-    const doc = await Url.findOne({shortened: val });
+    const doc = await Url.findOne({short_url: val });
     if (!doc) {
       return res.json({error: 'No short URL found' });
     }
-    return res.redirect(doc.ogurl);
+    return res.redirect(doc.original_url);
   } 
   catch (err) {
     console.error(err);
